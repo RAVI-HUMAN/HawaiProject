973dd8fe876a701b80125cce25ef693c32c757ff
RaviHuman <1062222764@mitwpu.edu.in>
2024-12-15 11:31:52 +0530

    Uploading files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Features from './pages/Features'
+import Index from './pages/Index'
+import Project from './pages/Project'
+import Quote from './pages/Quote'
+import Service from './pages/Service'
+import Team from './pages/Team'
+import Testimonial from './pages/Testimonial'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Index/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/Features' element={<Features/>}/>
+        <Route path='/Project' element={<Project/>}/>
+        <Route path='/Ouote' element={<Quote/>}/>
+        <Route path='/Service' element={<Service/>}/>
+        <Route path='/Team' element={<Team/>}/>
+        <Route path='/Testimonial' element={<Testimonial/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
